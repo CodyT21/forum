8f19326f50caf3371d3e9bc1bca36832b634f002
Cody Thomas <cody.t.thomas@gmail.com>
2023-02-13 22:09:04 -0700

    Fixed syntax errors in test and implemented user signin requirements

--- test/forum_test.rb
+++ test/forum_test.rb
@@ -14,7 +14,6 @@ class Forum < Minitest::Test
     @db.clear
 
     # test data
-    @db.create_new_category('Test Category', 100)
     @db.add_user('Test User', 'password')
     @db.add_post('Test Post', 'This is a test post.', 1)
     @db.add_comment_to_post(1, 1, 'Test comment')
@@ -25,7 +24,8 @@ class Forum < Minitest::Test
   end
   
   def test_user_session
-    'rack.session' => { user_id: 'Test User' }
+    user = @db.find_user('Test User', 'password')
+    { 'rack.session' => { user: user } }
   end
 
   def teardown
@@ -45,7 +45,6 @@ class Forum < Minitest::Test
     get last_response['Location']
     assert_equal 200, last_response.status
     assert_includes last_response.body, '<h1>Forum Posts</h1>'
-    assert_includes last_response.body
     assert_includes last_response.body, post[:title]
     assert_includes last_response.body, post[:author]
   end
@@ -54,10 +53,10 @@ class Forum < Minitest::Test
     post = @db.find_post(1)
     comment = @db.find_comment(1)
     
-    get '/posts/1'
+    get '/posts/1', {}, test_user_session
     assert_equal 302, last_response.status
     
-    get last_response['Location']
+    get last_response['Location'], {}
     assert_equal 200, last_response.status
     assert_includes last_response.body, "<h2>#{post[:title]}</h2>"
     assert_includes last_response.body, "<p>#{post[:content]}</p>"
@@ -67,50 +66,50 @@ class Forum < Minitest::Test
   end
 
   def test_add_new_post_page
-    get '/posts/new'
+    get '/posts/new', {}, test_user_session
     assert_equal 200, last_response.status
-    assert_includes last_response.body, '<h2>Add New Post</h2>'
+    assert_includes last_response.body, '<h2>Add a New Post</h2>'
     assert_includes last_response.body, %q(<input type="text" name="title" value="")
     assert_includes last_response.body, %q(<textarea name="content")
-    assert_includes last_response.body, %q(<input type="submit)
+    assert_includes last_response.body, %q(<input type="submit")
   end
   
   def test_add_new_post
-    post '/posts', { title: 'New Post', content: 'This is a new post.', user_id: 1 }
+    post '/posts', { title: 'New Post', content: 'This is a new post.', user_id: 1 }, test_user_session
     assert_equal 302, last_response.status
-    assert_equal 'Post added succesffuly.', session[:message]
+    assert_equal 'Post added successfully.', session[:message]
     
     new_post = @db.find_post(2)
     get last_response['Location']
     assert_equal 200, last_response.status
-    assert_includes last_response.body, post[:title]
-    assert_includes last_response.body, post[:author]
+    assert_includes last_response.body, new_post[:title]
+    assert_includes last_response.body, new_post[:author]
   end
 
   def test_add_new_post_invalid_title
-    post '/posts', { title: '', content: 'This is a new post.', user_id: 1 }
-    assert_equal last_response.body, 'Title must be between 1 and 100 characters.'
+    post '/posts', { title: '', content: 'This is a new post.', user_id: 1 }, test_user_session
+    assert_includes last_response.body, 'Title must be between 1 and 100 characters.'
   end
 
   def test_add_new_post_invalid_content
-    post '/posts', { title: 'New Post', content: '', user_id: 1 }
-    assert_equal last_response.body, 'Posts must have at least one character of content.'
+    post '/posts', { title: 'New Post', content: '', user_id: 1 }, test_user_session
+    assert_includes last_response.body, 'Posts must have at least one character of content.'
   end
 
   def test_render_post_edit_page
     post = @db.find_post(1)
     
-    get '/posts/1/edit'
+    get '/posts/1/edit', {}, test_user_session
     assert_equal 200, last_response.status
     assert_includes last_response.body, "<h2>Editing Post: #{post[:title]}</h2>"
     assert_includes last_response.body, %Q(<input type="text" name="title" value="#{post[:title]}")
     assert_includes last_response.body, "#{post[:content]}</textarea>"
-    assert_includes last_response.body, %q(<input type="submit)
+    assert_includes last_response.body, %q(<input type="submit")
   end
 
   def test_edit_post
     original_post = @db.find_post(1)
-    post '/posts/1', { title: 'New Title', content: 'This is an updated test post.', user_id: 1 }
+    post '/posts/1', { title: 'New Title', content: 'This is an updated test post.', user_id: 1 }, test_user_session
     assert_equal 302, last_response.status
     assert_equal 'The post was updated successfully.', session[:message]
     
@@ -119,14 +118,12 @@ class Forum < Minitest::Test
     assert_equal 200, last_response.status
     refute_includes last_response.body, original_post[:title]
     assert_includes last_response.body, updated_post[:title]
-    refute_includes last_response.body, original_post[:content]
-    assert_includes last_response.body, updated_post[:content]
     assert_includes last_response.body, original_post[:date]
     assert_includes last_response.body, updated_post[:update_date]
   end
 
   def test_edit_post_without_change
-    post '/posts/1', { title: 'Post Title', content: 'This is a test post.', user_id: 1 }
+    post '/posts/1', { title: 'Test Post', content: 'This is a test post.', user_id: 1 }, test_user_session
     assert_equal 302, last_response.status
     assert_equal 'The post was not changed.', session[:message]
     
@@ -137,7 +134,7 @@ class Forum < Minitest::Test
   def test_delete_post
     post = @db.find_post(1)
     
-    post '/posts/1/delete'
+    post '/posts/1/delete', {}, test_user_session
     assert_equal 302, last_response.status
     assert_equal 'The post was successfully deleted.', session[:message]
     
@@ -149,11 +146,11 @@ class Forum < Minitest::Test
   def test_add_comment
     post = @db.find_post(1)
     
-    get '/posts/1/comments'
+    get '/posts/1/comments', {}, test_user_session
     assert_includes last_response.body, %q(<textarea name="content")
     assert_includes last_response.body, %q(<input type="submit")
     
-    post '/posts/1/comments', { comment: 'This is a test comment.', user_id: 1 }
+    post '/posts/1/comments', { content: 'This is a test comment.', user_id: 1 }
     assert_equal 302, last_response.status
     assert_equal 'Comment added successfully.', session[:message]
     
@@ -163,24 +160,24 @@ class Forum < Minitest::Test
   end
 
   def test_add_comment_invalid_content
-    post '/posts/1/comments', { comment: '', user_id: 1 }
-    assert_inclues last_response.body, 'Comment cannot be left empty.'
+    post '/posts/1/comments', { content: '', user_id: 1 }, test_user_session
+    assert_includes last_response.body, 'Comment cannot be left empty.'
   end
 
   def test_render_comment_edit_page
     post = @db.find_post(1)
     comment = @db.find_comment(1)
     
-    get '/posts/1/comments/1/edit'
+    get '/posts/1/comments/1/edit', {}, test_user_session
     assert_equal 200, last_response.status
     assert_includes last_response.body, "<h2>Editing Comment for Post: #{post[:title]}</h2>"
     assert_includes last_response.body, "#{comment[:content]}</textarea>"
-    assert_includes last_response.body, %q(<input type="submit)
+    assert_includes last_response.body, %q(<input type="submit")
   end
 
   def test_edit_comment
     original_comment = @db.find_comment(1)
-    post '/posts/1/comments', { content: 'Updated test comment', user_id: 1 }
+    post '/posts/1/comments/1', { content: 'Updated test comment', user_id: 1 }, test_user_session
     assert_equal 302, last_response.status
     assert_equal 'The comment was updated successfully.', session[:message]
     
@@ -194,7 +191,7 @@ class Forum < Minitest::Test
   end
 
   def test_edit_comment_without_change
-    post '/posts/1/comments', { content: 'Test comment', user_id: 1 }
+    post '/posts/1/comments/1', { content: 'Test comment', user_id: 1 }, test_user_session
     assert_equal 302, last_response.status
     assert_equal 'The comment was not changed.', session[:message]
     
@@ -205,7 +202,7 @@ class Forum < Minitest::Test
   def test_delete_comment
     comment = @db.find_comment(1)
     
-    post '/posts/1/delete'
+    post '/posts/1/comments/1/delete', {}, test_user_session
     assert_equal 302, last_response.status
     assert_equal 'The comment was successfully removed.', session[:message]
     
@@ -215,13 +212,13 @@ class Forum < Minitest::Test
   end
   
   def test_invalid_post_id
-    get '/posts/10/comments'
+    get '/posts/10/comments', {}, test_user_session
     assert_equal 302, last_response.status
     assert_equal 'Post does not exist.', session[:message]
   end
   
   def test_invalid_comment_id
-    get '/posts/1/comments/5/edit'
+    get '/posts/1/comments/5/edit', {}, test_user_session
     assert_equal 302, last_response.status
     assert_equal 'Comment does not exist.', session[:message]
   end
